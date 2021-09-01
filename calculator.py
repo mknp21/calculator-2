5abfd63eb3e23a5cca579e31d53f42d5b910dcc4
Mika Patterson <mknpatt21@gmail.com>
2021-09-01 22:41:45 +0000

    Add numbers

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+
+# Replace this with your code
+# repeat forever:
+#     read input
+#     tokenize input
+#         if the first token is "q":
+#             quit
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
