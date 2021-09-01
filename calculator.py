13b97368871276358e211ff5cc6821cc38b50271
Mika Patterson <mknpatt21@gmail.com>
2021-09-01 23:37:20 +0000

    checking for wrong ifs

--- calculator.py
+++ calculator.py
@@ -5,36 +5,45 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
-# repeat forever:
+    # creating list with math options
+    math_options = ["+", "-", "*", "/", "s", "c", "p", "m"]
+    # repeat forever:
     while True:
 #     read input
         enter_nums = input("> ")
 #     tokenize input
         split_nums = enter_nums.split(' ')
+#assigning num1 and num2
+        operator = split_nums[0]
+        num1 = split_nums[1]
+        num2 = split_nums[2]
 #         if the first token is "q":
-        if split_nums[0] == "q":
+        if operator == "q":
 #             quit
             break
-
+        if num1 != num1.isdigit:
+            print("Sorry, that's not a valid entry!")
+        # if the num parameters for each given operand is not an integer...
+        if num1.isdigit() or num2.isdigit():
     # A place to store the return value of the math function we call,
     # to give us one clear place where that result is printed.
         result = None
 #        (decide which math function to call based on first token)
 #        if the first token is 'pow':
-        elif split_nums[0] == "+":   
+        elif operator == "+":   
 #            call the power function with the other two tokens
             result = add(float(num1), float(num2))
-        elif split_nums[0] == "-":
+        elif operator == "-":
             result = subtract(float(num1), float(num2))
-        elif split_nums[0] == "*":
+        elif operator == "*":
             result = multiply(float(num1), float(num2))
-        elif split_nums[0] == "/":
+        elif operator == "/":
             result = divide(float(num1), float(num2))
-        elif split_nums[0] == "s":
+        elif operator == "s":
             result = square(float(num1))
-        elif split_nums[0] == "c":
+        elif operator == "c":
             result = cube(float(num1))
-        elif split_nums[0] == "p":
+        elif operator == "p":
             result = power(float(num1))
-        elif split_nums[0] == "m":
+        elif operator == "m":
             result = mod(float(num1), float(num2))
